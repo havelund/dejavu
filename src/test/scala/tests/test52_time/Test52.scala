05a0728d12e3c976bf9f3e00cae8004564c52196
khavelun <klaus.havelund@jpl.nasa.gov>
2020-03-04 12:34:16 -0800

    added test case for H and P

--- src/test/scala/tests/test52_time/Test52.scala
+++ src/test/scala/tests/test52_time/Test52.scala
@@ -11,15 +11,15 @@ class Test52 extends TestCase {
   val log2 = s"$TEST/log2.timed.csv"
   val log3 = s"$TEST/log3.timed.csv"
 
-     @Test def test1(): Unit = {
-       Verify(spec,log1, "3", "debug")
-       checkResults()
-    }
+  @Test def test1(): Unit = {
+    Verify(spec, log1, "3", "debug")
+    checkResults()
+  }
 
-    @Test def test2(): Unit = {
-      Verify(spec,log2, "3", "debug")
-      checkResults(3,4)
-    }
+  @Test def test2(): Unit = {
+    Verify(spec, log2, "3", "debug")
+    checkResults(3, 4)
+  }
 
   @Test def test3(): Unit = {
     Verify(spec, log3, "3", "debug")
